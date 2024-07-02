c012e1e23d6b5bf36ebaa54ac5d29be226a75130
Mayar <maimagdy238@gmail.com>
2024-07-02 17:35:53 +0300

    delete php materials

--- PHP/Day 4/Day4/CRUD/config.php
+++
@@ -1,8 +0,0 @@
-<?php
- $server_name = "localhost";
- $username = "root";
- $password = "";
- $database_name = "day_4_swf";
-
-
-$connection =   mysqli_connect($server_name, $username, $password, $database_name);
